acc8662ac2be7b48d5aedfb915a5a67ae0792dc4
ilyobilyo <ilito20000@gmail.com>
2022-10-04 16:41:03 +0300

    Add NewsIndexViewModel for SearchedNews

---
+++ WorldNewsApp/Models/News/NewsIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NewsWorld.Core.ServiceModels.News;
+
+namespace WorldNewsApp.Models.News
+{
+    public class NewsIndexViewModel
+    {
+        public List<NewsServiceModel> News { get; set; }
+        public string SearchedMessage { get; set; }
+    }
+}
