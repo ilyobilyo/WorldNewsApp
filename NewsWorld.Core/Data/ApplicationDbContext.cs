2e857ea10fc5554832a942fef91e693b949cb880
ilyobilyo <ilito20000@gmail.com>
2022-10-05 21:29:02 +0300

    Add Seed for universities from API

---
+++ NewsWorld.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NewsWorld.Core.Data.Configure;
+using NewsWorld.Core.Data.Universities;
+
+namespace NewsWorld.Core
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<University> Universities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.ApplyConfiguration(new UniversityConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ NewsWorld.Core/Data/Configure/UniversityConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewsWorld.Core.Data.Universities;
+using NewsWorld.Core.ServiceModels.JsonObjects;
+using Newtonsoft.Json;
+
+namespace NewsWorld.Core.Data.Configure
+{
+    public class UniversityConfiguration : IEntityTypeConfiguration<University>
+    {
+        private readonly string baseUrl = "http://universities.hipolabs.com/search";
+
+        public void Configure(EntityTypeBuilder<University> builder)
+        {
+            List<University> universities =  GetUniversities();
+
+            builder.HasData(universities);
+        }
+
+        private List<University> GetUniversities()
+        {
+            var universities = new List<University>();
+
+            using (var client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage();
+
+                request.RequestUri = new Uri(baseUrl);
+
+                request.Method = HttpMethod.Get;
+
+                HttpResponseMessage response =  client.Send(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var responseString =  response.Content.ReadAsStringAsync();
+
+                    var jsonObj = JsonConvert.DeserializeObject<List<UniversityJsonObject>>(responseString.Result);
+
+                    foreach (var uni in jsonObj)
+                    {
+                        var university = new University()
+                        {
+                            Alpha_Two_Code = uni.Alpha_Two_Code,
+                            Country = uni.Country,
+                            Name = uni.Name,
+                            StateProvince = uni.StateProvince,
+                            Domain = uni.Domains[0],
+                            WebPage = uni.Web_Pages[0],
+                        };
+
+                        universities.Add(university);
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return universities;
+        }
+    }
+}
