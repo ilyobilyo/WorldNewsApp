9dbf92aba18dc3a7ec44340384245d146f249397
ilyobilyo <ilito20000@gmail.com>
2022-10-05 21:27:25 +0300

    Add Data Entity for University and create Database

---
+++ NewsWorld.Core/Data/Migrations/20221005162901_InitialCreateUniversities.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorldNewsApp.Data.Migrations
+{
+    public partial class InitialCreateUniversities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Alpha_Two_Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StateProvince = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniversitiesDomains",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Domain = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UniversityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniversitiesDomains", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniversitiesDomains_Universities_UniversityId",
+                        column: x => x.UniversityId,
+                        principalTable: "Universities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniversitiesWebPages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UniversityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniversitiesWebPages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniversitiesWebPages_Universities_UniversityId",
+                        column: x => x.UniversityId,
+                        principalTable: "Universities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversitiesDomains_UniversityId",
+                table: "UniversitiesDomains",
+                column: "UniversityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversitiesWebPages_UniversityId",
+                table: "UniversitiesWebPages",
+                column: "UniversityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UniversitiesDomains");
+
+            migrationBuilder.DropTable(
+                name: "UniversitiesWebPages");
+
+            migrationBuilder.DropTable(
+                name: "Universities");
+        }
+    }
+}
---
+++ NewsWorld.Core/Data/Universities/University.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsWorld.Core.Data.Universities
+{
+    public class University
+    {
+        public University()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Alpha_Two_Code { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Country { get; set; }
+
+        public string? StateProvince { get; set; }
+
+        public string? Domain { get; set; }
+
+        public string WebPage { get; set; }
+    }
+}
