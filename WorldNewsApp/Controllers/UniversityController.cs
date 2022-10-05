777ebca6033318e281aa5203381b7f91f0c756d5
ilyobilyo <ilito20000@gmail.com>
2022-10-05 21:29:36 +0300

    Add UniversityController

---
+++ WorldNewsApp/Controllers/UniversityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsWorld.Core.Contracts;
+
+namespace WorldNewsApp.Controllers
+{
+    public class UniversityController : Controller
+    {
+        private readonly IUniversityService universityService;
+
+        public UniversityController(IUniversityService universityService)
+        {
+            this.universityService = universityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var universities = universityService.GetUniversitiesSeed();
+
+            return View();
+        }
+    }
+}
---
+++ WorldNewsApp/Models/Universities/UniversityIndexModel.cs
@@ -0,0 +1,10 @@
+﻿using NewsWorld.Core.ServiceModels.University;
+
+namespace WorldNewsApp.Models.Universities
+{
+    public class UniversityIndexModel
+    {
+        public List<UniversityServiceModel> Universities { get; set; }
+        public string SearchedMessage { get; set; }
+    }
+}
