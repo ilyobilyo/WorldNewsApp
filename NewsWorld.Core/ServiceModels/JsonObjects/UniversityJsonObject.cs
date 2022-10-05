b7db2bcf90a8f0d24b7b5845fd9d41fb52e00ed9
ilyobilyo <ilito20000@gmail.com>
2022-10-05 21:29:17 +0300

    Create DTOs for universities

---
+++ NewsWorld.Core/ServiceModels/JsonObjects/UniversityJsonObject.cs
@@ -0,0 +1,17 @@
+﻿namespace NewsWorld.Core.ServiceModels.JsonObjects
+{
+    public class UniversityJsonObject
+    {
+        public string Name { get; set; }
+
+        public string Alpha_Two_Code { get; set; }
+
+        public string Country { get; set; }
+
+        public string StateProvince { get; set; }
+
+        public IList<string> Web_Pages { get; set; }
+
+        public IList<string> Domains { get; set; }
+    }
+}
---
+++ NewsWorld.Core/ServiceModels/University/UniversityServiceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NewsWorld.Core.ServiceModels.University
+{
+    public class UniversityServiceModel
+    {
+        public string Name { get; set; }
+
+        public string AlphaTwoCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string StateProvince { get; set; }
+
+    }
+}
