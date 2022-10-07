b0875d4493f8897110b44d6c88aee1634de62e9a
ilyobilyo <ilito20000@gmail.com>
2022-10-07 16:18:24 +0300

    Add PageingModels

---
+++ NewsWorld.Core/Pageing.Models/PageingList.cs
@@ -0,0 +1,21 @@
+﻿namespace WorldNewsApp.Models.Pageing
+{
+    public class PageingList<T> : List<T>
+    {
+        public PageingList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public int PageIndex { get; init; }
+
+        public int TotalPages { get; init; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+    }
+}
---
+++ NewsWorld.Core/Pageing.Models/PageingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using NewsWorld.Core.ServiceModels.University;
+
+namespace WorldNewsApp.Models.Pageing
+{
+    public class PageingViewModel
+    {
+        public PageingList<UniversityServiceModel> Universities { get; set; }
+
+        public string SearchedMessage { get; set; }
+    }
+}
