45fdb03f87b8ed3e0bd3d5b3047e9c68e0361b66
ilyobilyo <ilito20000@gmail.com>
2022-10-07 16:17:55 +0300

    Add Database Repository

---
+++ NewsWorld.Core/Data/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsWorld.Core.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ NewsWorld.Core/Data/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewsWorld.Core.Data.Common
+{
+    public class Repository : IRepository
+    {
+        protected DbContext Context { get; set; }
+
+        protected DbSet<T> DbSet<T>() where T : class
+        {
+            return this.Context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>().AsQueryable();
+        }
+
+        public int SaveChanges()
+        {
+            return this.Context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await this.Context.SaveChangesAsync();
+        }
+    }
+}
