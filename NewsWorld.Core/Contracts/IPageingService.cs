eca4495dedb9d66119ad08621b1d3a46d8a5b1de
ilyobilyo <ilito20000@gmail.com>
2022-10-07 16:17:01 +0300

    Add PageingService for universities

---
+++ NewsWorld.Core/Contracts/IPageingService.cs
@@ -0,0 +1,9 @@
+﻿using WorldNewsApp.Models.Pageing;
+
+namespace NewsWorld.Core.Contracts
+{
+    public interface IPageingService
+    {
+        Task<PageingViewModel> GetUniversitiesPerPage(int pageNumber, string country = null);
+    }
+}
---
+++ NewsWorld.Core/Services/PageingService.cs
@@ -0,0 +1,34 @@
+﻿using NewsWorld.Core.Contracts;
+using NewsWorld.Core.ServiceModels.University;
+using WorldNewsApp.Models.Pageing;
+
+namespace NewsWorld.Core.Services
+{
+    public class PageingService : IPageingService
+    {
+        private readonly IUniversityService universityService;
+
+        public PageingService(IUniversityService universityService)
+        {
+            this.universityService = universityService;
+        }
+
+        public async Task<PageingViewModel> GetUniversitiesPerPage(int pageNumber, string country = null)
+        {
+            var totalCountUniversities = await universityService.GetTotalCountOfUniversities(country);
+
+            var pageSize = 10;
+
+            var universities = await universityService.GetUniversities(pageNumber, pageSize, country);
+
+            var pageList = new PageingList<UniversityServiceModel>(universities, totalCountUniversities, pageNumber, pageSize);
+
+            var pageingViewModel = new PageingViewModel()
+            {
+                Universities = pageList,
+            };
+
+            return pageingViewModel;
+        }
+    }
+}
